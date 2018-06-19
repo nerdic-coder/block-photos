b310496950b2aefcf75244fd23671a245630b925
TheNerdicCoder <johan@efdapps.com>
2018-06-20 01:14:37 +0300

    Init project files with react and ionic

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <ion-app>
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>React Block</ion-title>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content>
+          <ion-card>
+            <ion-card-content>
+              <ion-card-title>
+                Work in progress
+              </ion-card-title>
+              <p>
+                More coming soon...
+              </p>
+            </ion-card-content>
+          </ion-card>
+        </ion-content>
+      </ion-app>
+    );
+  }
+}
+
+export default App;
