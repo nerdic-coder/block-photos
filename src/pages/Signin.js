ce5cf242f20f767d7a891d9c28bab60dc3d5963f
TheNerdicCoder <johan@efdapps.com>
2018-10-26 00:21:12 +0300

    Fix signing redirect for web app

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -7,7 +7,8 @@ import {
   DEFAULT_SCOPE,
   isUserSignedIn,
   isSignInPending,
-  handlePendingSignIn
+  handlePendingSignIn,
+  redirectToSignIn
 } from 'blockstack';
 import PresentingService from '../services/PresentingService.js';
 import isElectron from 'is-electron';
@@ -16,6 +17,10 @@ export default class Signin extends Component {
 
   static propTypes = {
     history: PropTypes.any
+  };
+
+  state = {
+    redirected: false
   };
 
   constructor(props) {
@@ -39,21 +44,24 @@ export default class Signin extends Component {
 
   handleSignIn(e) {
     e.preventDefault();
-    this.present.loading('Waiting for authentication...', 60000, true);
 
-    let appDomain = 'http://localhost:9876';
     if (!isElectron()) {
-      appDomain = 'https://app.block-photos.com'
+      redirectToSignIn();
+      this.setState({ redirected: true });
+    } else {
+      this.present.loading('Waiting for authentication...', 60000, true);
+      let appDomain = 'http://localhost:9876';
+      const transitPrivateKey = generateAndStoreTransitKey();
+      const redirectURI = appDomain + '/callback';
+      const manifestURI = appDomain + '/manifest.json';
+      const scopes = DEFAULT_SCOPE;
+      var authRequest = makeAuthRequest(transitPrivateKey, redirectURI, manifestURI, scopes, appDomain);
+      redirectToSignInWithAuthRequest(authRequest);
     }
-    const transitPrivateKey = generateAndStoreTransitKey();
-    const redirectURI = appDomain + '/callback';
-    const manifestURI = appDomain + '/manifest.json';
-    const scopes = DEFAULT_SCOPE;
-    var authRequest = makeAuthRequest(transitPrivateKey, redirectURI, manifestURI, scopes, appDomain);
-    redirectToSignInWithAuthRequest(authRequest);
   }
 
   render() {
+    const { redirected } = this.state;
     return (
       <React.Fragment>
         <ion-header>
@@ -65,12 +73,14 @@ export default class Signin extends Component {
           <ion-card>
             <ion-card-content>
               <h1>Welcome to Block Photos!</h1>
-              <ion-button expand="block"
-                id="signin-button"
-                onClick={(event) => this.handleSignIn(event)}
-              >
-                Sign In with Blockstack
-          </ion-button>
+              {redirected ? ( <p>Thanks for logging in! You can close this window now.</p>) : (
+                <ion-button expand="block"
+                  id="signin-button"
+                  onClick={(event) => this.handleSignIn(event)}
+                >
+                  Sign In with Blockstack
+                </ion-button>
+              )}
             </ion-card-content>
           </ion-card>
         </ion-content>
