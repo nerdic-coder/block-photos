41ed344b579aec564bccbce97601e7aa569fd301
TheNerdicCoder <johan@efdapps.com>
2018-10-30 00:25:45 +0200

    Autotests for Cache service

---
+++ src/services/CacheService.test.js
@@ -0,0 +1,69 @@
+import CacheService from './CacheService';
+import * as localForage from "localforage";
+
+jest.mock('localforage');
+
+describe('CacheService Test Suites', () => {
+
+  it('initialize without crashing', () => {
+    new CacheService();
+  });
+
+  it('get pictures list', async () => {
+    const mockResponse = [{
+      "id": 'test123.jpg',
+      "uploadedDate": "2018-07-11T21:58:39.754Z"
+    }];
+
+    localForage.getItem.mockReturnValue(Promise.resolve(JSON.stringify(mockResponse)));
+
+    const cacheService = new CacheService();
+    const response = await cacheService.getItem('cachedPicturesList');
+
+    const jsonResponse = JSON.parse(response);
+    expect(Array.isArray(jsonResponse)).toBe(true);
+    expect(jsonResponse).toEqual(mockResponse);
+
+  });
+
+  it('get empty pictures list', async () => {
+    const mockResponse = [];
+
+    localForage.getItem.mockReturnValue(Promise.resolve(JSON.stringify(mockResponse)));
+
+    const cacheService = new CacheService();
+    const response = await cacheService.getItem('cachedPicturesList');
+
+    const jsonResponse = JSON.parse(response);
+    expect(Array.isArray(jsonResponse)).toBe(true);
+    expect(jsonResponse).toEqual(mockResponse);
+
+  });
+
+  it('get null pictures list', async () => {
+
+    const mockResponse = null;
+    
+    localForage.getItem.mockReturnValue(Promise.resolve(JSON.stringify(mockResponse)));
+
+    const cacheService = new CacheService();
+    const response = await cacheService.getItem('cachedPicturesList');
+
+    const jsonResponse = JSON.parse(response);
+    expect(Array.isArray(jsonResponse)).toBe(false);
+    expect(jsonResponse).toBe(null);
+
+  });
+
+  it('cache picture', async () => {
+    const cacheService = new CacheService();
+    await cacheService.setItem('test1.png', [{
+      filename: 'test1.png',
+      data: 'trash'
+    }]);
+
+    // const response = await cacheService.getItem('test1.png');
+  
+  });
+
+});
